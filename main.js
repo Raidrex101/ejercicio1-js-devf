a3cfb59fd38a6f85b12c5a295b98dc280f955a37
hector castro <raidrex@hotmail.com>
2024-03-17 21:04:28 -0600

    trabajos actividad 1

--- main.js
+++ main.js
@@ -17,7 +17,7 @@ if (eresBellisimo === "si") {
 
 
 
- //("------Ejercicio 2------")
+//("------Ejercicio 2------")
 
 
 
@@ -161,7 +161,7 @@ if (topping === "oreo") {
 
 
 
-const precios = {
+/* const precios = {
     "course": 4999,
     "carrera": 3999,
     "master": 2999
@@ -178,22 +178,127 @@ const duracion = {
     "carrera": 6,
     "master": 12
 }
+ */
+const nivel = 1 //Number(prompt("Elije un nivel \n 1. Course $4999 \n 2. Carrera $3999 \n 3. Master $2999 "))
 
-let costo = {
-    precios, 
-    becas,
-    duracion,
-    finalPrice: function () {
-        return this.price - (this.price * this.discount)
+const beca = 4 //prompt("Tiene alguna de estas becas? \n 1. Facebook 20% \n 2. Google 15% \n 3. Jesua 50% \n 4. No tengo beca ")
+
+
+function costoPrograma(nivel, beca) {
+    let costoMensual = 0
+    
+    if (nivel === 1) {
+        costoMensual = 4999
+
+    }else if (nivel === 2) {
+        costoMensual = 3999
+
+    }else if (nivel === 3) {
+        costoMensual = 2999
+    }        
+
+    if (beca === 1) {
+        costoMensual -= costoMensual * 0.2
+
+    }else if (beca === 2) {
+        costoMensual -= costoMensual * 0.15
+
+    }else if (beca === 3) {
+        costoMensual -= costoMensual * 0.50
+
+    }else if (beca === 4) {
+        costoMensual
+    }        
+
+    return costoMensual
+}
+
+function calcularCostoTotal (nivel, costoMensual) {
+    let costoFinal = 0
+
+    if (nivel = 1) {
+        costoFinal = costoMensual * 2
+    }else if (nivel = 2) {
+        costoFinal = costoMensual * 6
+    }else if (nivel = 3) {
+        costoFinal = costoMensual * 12
     }
+    return costoFinal
 }
 
 
 
+const costoMensual = costoPrograma(nivel, beca)
+const costoTotal = calcularCostoTotal(nivel, costoMensual)
+
+console.log("El costo mensual del programa es " + costoMensual)
+console.log("El costo total del programa es " + costoTotal)
+
+// solo sirve para igualdades, un ejemplo de como hacer el mismo else if de la funcion pero con otro metodo
+/* switch (nivel) {
+    case 1:
+        costoMensual = 2999
+        break;
+    case 2:
+        costoMensual = 3999
+        break;
+    case 3:
+        costoMensual = 4999        
+        break;
+    default:
+        break;
+} */
+
+
 //("-----Ejercicio 11-----")
+
+
+
 
 /* 11. Realizar un programa que ayude a calcular el total a pagar de acuerdo a la distancia recorrida por un vehículo con cargo extra por los litros consumidos, 
 tomando en consideración lo siguiente:
 Si el vehículo es “coche”, el precio kilometro ha de ser 0.20, si es “moto” ha de ser 0.10 y si es “autobús” 0.5.
 Si los litros consumidos están entre 0 y 100 se ha de añadir 5 al costo total, si es mayor la cantidad de litros consumidos se ha de añadir 10 al total. Considere qué:
 total a pagar = (precio kilometro x kms recorridos) + extra por litros consumidos. */
+
+/* const litros  10
+
+ kilometros por litro = coche 2  moto 1 autobus 5
+
+const vehiculos = {
+    "coche": 0.20,
+    "moto": 0.10,
+    "autobus":0.5
+}
+ */
+
+
+const transporte = 2//Number(prompt("Que vehiculo desearia usar? \n 1. Coche $0.20/km \n 2. Moto $0.10/km \n 3. Autobus 0.5/km"))
+const distancia = 100//Number(prompt("A que distania en km esta su destino?"))
+
+function precioKm(transporte, distancia) {
+    let costo = 0
+
+    if (transporte === 1) {
+        costo = 0.20
+
+    }else if (transporte === 2) {
+        costo = 0.10
+
+    }else if (transporte === 3) {
+        costo = 0.5
+    }
+
+    let precioTotal = costo * distancia
+
+    if (distancia <= 100) {
+        precioTotal += 5
+    }else {
+        precioTotal += 10
+    }
+    
+    return precioTotal
+}
+const precioTotal = precioKm(transporte, distancia)
+console.log (precioTotal)
+
